bb68a67a939a3dd03fc48afa32e6af1cac188f98
Xdudu <liuyuewuyu@yeah.net>
2019-05-14 13:19:40 +0200

    Updates

---
+++ precache-manifest.8fe981d6f8859a9464bfa4c7be202522.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808737f7f698822136fa2c5434b4bd65",
+    "url": "/shadow-and-the-dancer/index.html"
+  },
+  {
+    "revision": "f54ccb1b94b796b6ec78",
+    "url": "/shadow-and-the-dancer/static/css/main.6da18f89.chunk.css"
+  },
+  {
+    "revision": "75c217fd0e0250d9c3b2",
+    "url": "/shadow-and-the-dancer/static/js/2.2f2b22ab.chunk.js"
+  },
+  {
+    "revision": "f54ccb1b94b796b6ec78",
+    "url": "/shadow-and-the-dancer/static/js/main.ba4059b3.chunk.js"
+  },
+  {
+    "revision": "2eba07f8c22525fa3b3c",
+    "url": "/shadow-and-the-dancer/static/js/runtime~main.c3e9552e.js"
+  }
+]);
